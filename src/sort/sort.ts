5fef7c9ca0f6ce05e1537bb416cb9e7f418d642f
Jiantao Wu （吴建涛） <jiantao.wu@cerence.com>
2023-06-05 17:54:03 +0800

    added insertion sort

--- src/sort/sort.ts
+++ src/sort/sort.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import { generateArray } from "./utils";
 
 // 生序
@@ -5,7 +6,7 @@ class Sort {
   private target: number[] = []
   constructor(target: number[]) {
     this.target = target
-    console.log(`original result: ${JSON.stringify(this.target)}`)
+    // console.log(`original result: ${JSON.stringify(this.target)}`)
   }
 
   selectionSort() {
@@ -26,8 +27,34 @@ class Sort {
     }
     return list
   }
+
+  insertionSort() {
+    const list = this.target.slice(0)
+    for(let i = 0; i < list.length; i++) {
+      for(let j = i + 1; j > 0 && list[j] < list[j-1]; j--) {
+        const min = list[j]
+        list[j] = list[j - 1]
+        list[j - 1] = min
+      }
+    }
+    return list
+  }
 }
 
 const sortInstance = new Sort(generateArray())
 
-console.log(`Selection Result: ${JSON.stringify(sortInstance.selectionSort())}`)
+class SortCompare {
+  static compare(fn: () => number[], title: string) {
+    const start = new Date()
+    const startDate =  dayjs(start).format('YYYY-MM-DD hh:mm:ss')
+    const result = fn.bind(sortInstance)()
+    const end = new Date()
+    const endDate = dayjs(end).format('YYYY-MM-DD hh:mm:ss')
+
+    // console.log(`${title} Result: ${JSON.stringify(result)}`)
+    console.log(`${title}: Operating time is`, end.getTime() - start.getTime(), `start:${startDate}, end: ${endDate}`)
+  }
+}
+
+SortCompare.compare(sortInstance.selectionSort, 'Selection')
+SortCompare.compare(sortInstance.insertionSort, 'Insertion')
--- src/sort/utils.ts
+++ src/sort/utils.ts
@@ -1,6 +1,6 @@
 export function generateArray() {
   const result = []
-  for (let index = 0; index < 10; index++) {
+  for (let index = 0; index < 100000; index++) {
     const number = Math.round(Math.random()*1000)
     result.push(number)
   }
