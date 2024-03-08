8587edeffd7e45292a92768f0f558f4313de8f9f
Jiantao Wu （吴建涛） <jiantao.wu@cerence.com>
2024-03-08 13:56:53 +0800

    added shell sort

--- src/sort/sort.ts
+++ src/sort/sort.ts
@@ -39,6 +39,43 @@ class Sort {
     }
     return list
   }
+
+  shellSort() {
+    const list = this.target.slice(0)
+    const {length} = list
+    let n = Math.floor(length / 4)
+    while(n >= 1) {
+      for(let i = n; i < length; i++) {
+        for(let j = i; j >= n && list[j] < list[j - n];j -= n) {
+          const temp = list[j]
+          list[j] = list[j - n]
+          list[j-n] = temp
+        }
+      }
+      n = Math.floor(n / 2)
+    }
+    return list
+  }
+
+  mergeSort() {
+    const list = this.target.slice(0)
+  }
+
+  private mergeSortSubMethod(list: number[]) {
+    const {length} = list
+    const mid = Math.floor(length / 2)
+    this.merge(0, mid, list)
+    this.merge(mid, length - 1, list)
+    this.sort(0, mid, length - 1, list)
+  }
+
+  private merge(left: number, right: number, list: number[]) {
+    
+  }
+
+  private sort(left: number, mid: number, right: number, list: number[]) {
+
+  }
 }
 
 const sortInstance = new Sort(generateArray())
@@ -58,3 +95,4 @@ class SortCompare {
 
 SortCompare.compare(sortInstance.selectionSort, 'Selection')
 SortCompare.compare(sortInstance.insertionSort, 'Insertion')
+SortCompare.compare(sortInstance.shellSort, 'Shell')
